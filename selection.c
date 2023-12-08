beaa972fd3ea25b6bd45506ec9f88979cdb9144c
smit umraliya <umaraliyasmeet12@gmail.com>
2023-12-08 15:05:35 +0530

    adding 1.cpp file

--- selection.c
+++ selection.c
@@ -11,14 +11,9 @@ int selection(int arr[])
                 arr[i] = arr[i] + arr[j];
                 arr[j] = arr[i] - arr[j];
                 arr[i] = arr[i] - arr[j];
-            }
-            
+            }  
         }  
-    }
-    for (int i = 0; i < 8; i++)
-    {
-        printf("%d ",arr[i]);
-    }
+    }  
 }
 
 int main()
@@ -26,6 +21,11 @@ int main()
     int arr[8] = {7, 21, 17, 75, 32, 3, 47, 11};
 
     selection(arr);
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d ",arr[i]);
+    }
 
     return 0;
 }
