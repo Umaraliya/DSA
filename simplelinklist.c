83af2dfd069cc0b79f83f9cc32342c4be5aac225
smit umraliya <umaraliyasmeet12@gmail.com>
2023-12-20 16:27:56 +0530

    adding 1.cpp file

--- simplelinklist.c
+++ simplelinklist.c
@@ -9,6 +9,49 @@ struct node
 
 struct node *head = NULL;
 
+void insertMiddle(int val)
+{
+    struct node *ptr = head, *p;
+    struct node *temp = malloc(sizeof(struct node));
+    temp->data = val;
+    temp->next = NULL;
+
+    int s,i=1;
+    
+    if (head == NULL)
+    {
+        head = temp;
+    }
+    else if(head->next == NULL)
+    {
+        head->next = temp; 
+    }
+    else
+    {
+        printf("Insert your Position : ");
+        scanf("%d",&s);
+
+        i = 1;
+
+        if (s == 1)
+        {
+            temp->next = head;
+            head = temp;
+        }
+        else{
+            while (i<s)
+            {
+                i++;
+                p = ptr;
+                ptr = ptr->next;
+            }
+
+        temp->next = ptr;
+        p->next = temp;
+        }
+    }
+}
+
 void insertEnd(int val)
 {
     struct node *ptr = head;
@@ -27,6 +70,63 @@ void insertEnd(int val)
     }
     ptr->next = temp;
     return;
+}
+
+void insertFirst(int val)
+{
+    struct node *ptr = head;
+    struct node *temp = malloc(sizeof(struct node));
+    temp->data = val;
+    temp->next = head;
+    head=temp;
+}
+void deleteFirst()
+{
+    struct node *ptr = head;
+    
+    if (head == NULL)
+    {
+        printf("\nList is empty\n");
+    }
+    else
+    {
+        head = head->next;
+    }    
+}
+
+void deleteMiddle()
+{
+    struct node *ptr = head, *p;
+
+    int s,i=1;
+
+    if (head == NULL)
+    {
+        printf("\nList is already empty");
+    }
+    else
+    {
+        printf("delete your position : ");
+        scanf("%d",&s);
+        i = 1;
+        if (s == 1)
+        {
+            head = ptr->next;
+            free(ptr);
+        }
+        else
+        {
+            while (i<s)
+            {
+                i++;
+                p = ptr;
+                ptr = ptr->next;
+            }
+        
+        p->next = ptr->next;
+        free(ptr);
+        }
+    }
 }
 
 void deleteEnd()
@@ -58,7 +158,7 @@ void deleteEnd()
 void display()
 {
     struct node *ptr = head;
-    
+
     if (head == NULL)
     {
         printf("List is empty");
@@ -82,7 +182,19 @@ int main()
     insertEnd(400);
     insertEnd(500);
     display();
+    insertMiddle(250);
+    printf("InsertMiddle\n");
+    display();
+    insertFirst(50);
+    printf("InsertFirst\n");
+    display();
+    deleteFirst();
+    printf("deleteFirst\n");
+    display();
+    deleteMiddle();
+    printf("deleteMiddle\n");
+    display();
     deleteEnd();
-    deleteEnd();
+    printf("deleteEnd\n");
     display();
 }
